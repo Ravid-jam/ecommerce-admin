afdd7b5a46deeab769edea9ef6ccf325f51ad30b
Ravid-jam <ravid.jam02@gmail.com>
2025-02-13 18:19:39 +0530

    feat: url changed.

--- src/components/config.ts
+++ src/components/config.ts
@@ -1,6 +1,6 @@
 const config = {
-  // API_URL: "https://ecommerce-rest-api-y2lw.onrender.com",
-  API_URL: "http://localhost:5000",
+  API_URL: "https://ecommerce-rest-api-y2lw.onrender.com",
+  // API_URL: "http://localhost:5000",
   // API_URL: "https://lucky-ray-nightshirt.cyclic.app",
 };
 
